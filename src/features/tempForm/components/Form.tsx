051ff44abed695a04a88737c40e716e470829f8e
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-01-29 02:59:27 +0000

    created the Stepper component

--- src/features/tempForm/components/Form.tsx
+++ src/features/tempForm/components/Form.tsx
@@ -2,16 +2,20 @@ import { MultiStepForm } from '@/features/tempForm/setupForm';
 import { Form1 } from '@/features/tempForm/components/Form1';
 import { Form2 } from '@/features/tempForm/components/Form2';
 import { Form3 } from '@/features/tempForm/components/Form3';
+import { Stepper } from '@/ui/Stepper';
 
 export function Form() {
   return (
-    <MultiStepForm
-      bounce
-      steps={[
-        { title: 'form1', component: <Form1 /> },
-        { title: 'form2', component: <Form2 /> },
-        { title: 'form3', component: <Form3 /> },
-      ]}
-    />
+    <div className="flex flex-col gap-8">
+      <Stepper />
+      <MultiStepForm
+        bounce
+        steps={[
+          { title: 'form1', component: <Form1 /> },
+          { title: 'form2', component: <Form2 /> },
+          { title: 'form3', component: <Form3 /> },
+        ]}
+      />
+    </div>
   );
 }
