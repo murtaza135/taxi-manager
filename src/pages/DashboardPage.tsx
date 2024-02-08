d2fd81a83b35f99aa465ee43194bd38e93851e1c
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-02-08 02:23:15 +0000

    added Table and DataTable components, needs restyling and restructuring

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,10 +1,28 @@
 import { Title } from '@/features/title/components/Title';
+import { DataTable } from '@/features/tempTable/DataTable';
+import { Payment, columns } from '@/features/tempTable/columns';
+
+const data: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '728ed530',
+    amount: 120,
+    status: 'success',
+    email: 'm2@example.com',
+  },
+];
 
 export function DashboardPage() {
   return (
     <>
       <Title title="Dashboard" />
-      <div>DashboardPage</div>
+      {/* <div>DashboardPage</div> */}
+      <DataTable columns={columns} data={data} />
     </>
   );
 }
