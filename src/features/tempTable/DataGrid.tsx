1eec73f78b0643b55fe851a4acbd509797fd9c32
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-02-09 03:10:40 +0000

    fleshed out DataGridCard

--- src/features/tempTable/DataGrid.tsx
+++ src/features/tempTable/DataGrid.tsx
@@ -2,7 +2,10 @@ import { DataGridCard } from '@/features/tempTable/DataGridCard';
 
 export function DataGrid() {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,minmax(15rem,1fr))] gap-4">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(13rem,1fr))] gap-4">
+      <DataGridCard />
+      <DataGridCard />
+      <DataGridCard />
       <DataGridCard />
       <DataGridCard />
       <DataGridCard />
--- src/features/tempTable/DataGridCard.tsx
+++ src/features/tempTable/DataGridCard.tsx
@@ -1,13 +1,31 @@
 export function DataGridCard() {
   return (
     <div className="h-full min-h-[27rem] rounded-lg overflow-hidden bg-achromatic-light dark:bg-achromatic-dark">
-      <div className="h-32 mb-16 relative">
+      <div className="h-28 mb-16 relative">
         <img src="/src/assets/images/sea.jpg" alt="sea" className="object-cover object-center h-full w-full" />
-        <img src="/src/assets/images/person.jpg" alt="person" className="h-32 w-32 object-cover object-center rounded-full absolute top-full left-1/2 -translate-x-16 -translate-y-16" />
+        <img src="/src/assets/images/person.jpg" alt="person" className="h-32 w-32 object-cover object-center rounded-full absolute top-full left-1/2 -translate-x-16 -translate-y-16 border-0 border-achromatic-light dark:border-achromatic-dark" />
       </div>
 
-      <div className="p-4">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, reiciendis!
+      <div className="px-6 pb-10 pt-4 space-y-6">
+        <div className="text-center">
+          <p className="text-2xl font-semibold">Jane Doe</p>
+          <p className="text-achromatic-dark/50 dark:text-achromatic-light/50">AB20 1CD</p>
+        </div>
+
+        <div className="space-y-3">
+          <div>
+            <p className="text-xs font-semibold text-achromatic-dark/50 dark:text-achromatic-light/50">Email</p>
+            <p>jane-doe@test.com</p>
+          </div>
+          <div>
+            <p className="text-xs font-semibold text-achromatic-dark/50 dark:text-achromatic-light/50">Phone Number</p>
+            <p>01234567890</p>
+          </div>
+          <div>
+            <p className="text-xs font-semibold text-achromatic-dark/50 dark:text-achromatic-light/50">Hire Contract</p>
+            <p>ABC123</p>
+          </div>
+        </div>
       </div>
     </div>
   );
