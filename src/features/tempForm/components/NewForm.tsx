4074e2520f23085fa79946a3564fa4a97b0a0c1e
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-02-18 03:09:26 +0000

    created Stepper components for MultiStepForm

--- src/features/tempForm/components/NewForm.tsx
+++ src/features/tempForm/components/NewForm.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Form1 } from '@/features/tempForm/components/Form1';
 import { Form2 } from '@/features/tempForm/components/Form2';
 import { Form3 } from '@/features/tempForm/components/Form3';
-import { MultiStepForm, MultiStepFormContent, Step } from '@/ui/MultiStepForm2';
+import { MultiStepForm, MultiStepFormContent, Step, MultiStepFormStepper, MultiStepFormStepperItem } from '@/ui/MultiStepForm2';
 import { FormSchemaType } from '@/features/tempForm/schema';
 
 export function NewForm() {
@@ -14,6 +14,12 @@ export function NewForm() {
 
   return (
     <MultiStepForm min={1} max={3}>
+      <MultiStepFormStepper>
+        <MultiStepFormStepperItem step={1} title="Form 1" />
+        <MultiStepFormStepperItem step={2} title="Form 2" />
+        <MultiStepFormStepperItem step={3} title="Form 3" />
+      </MultiStepFormStepper>
+
       <MultiStepFormContent className="flex justify-center items-start">
         <Step step={1}><Form1 /></Step>
         <Step step={2}><Form2 /></Step>
--- src/ui/MultiStepForm2.tsx
+++ src/ui/MultiStepForm2.tsx
@@ -21,6 +21,8 @@ type MultiStepFormContextValue<
   setDirection: React.Dispatch<React.SetStateAction<Direction>>;
   formState: Partial<TFormState>;
   updateFormState: (state: Partial<TFormState>) => void;
+  min: number;
+  max: number;
 };
 
 const MultiStepFormContext = React.createContext<MultiStepFormContextValue>(
@@ -74,8 +76,10 @@ function MultiStepForm({ min, max, initial, className, children }: MultiStepForm
       setDirection,
       formState: formStateObject,
       updateFormState,
+      min,
+      max,
     }),
-    [stepValue, setStep, direction, setDirection, formStateObject, updateFormState],
+    [stepValue, setStep, direction, setDirection, formStateObject, updateFormState, min, max],
   );
 
   return (
@@ -117,9 +121,76 @@ function Step({ step, children }: StepProps) {
   );
 }
 
+const MultiStepFormStepper = React.forwardRef<
+  HTMLOListElement,
+  React.HTMLAttributes<HTMLOListElement>
+>(({ className, children, ...props }, ref) => {
+  const temp = 1;
+
+  return (
+    <ol
+      ref={ref}
+      className={cn('flex flex-wrap mb-12', className)}
+      {...props}
+    >
+      {children}
+    </ol>
+  );
+});
+MultiStepFormStepper.displayName = 'MultiStepFormStepper';
+
+type MultiStepFormStepperItemProps = {
+  step: number;
+  title?: string;
+};
+
+const MultiStepFormStepperItem = React.forwardRef<
+  HTMLLIElement,
+  React.HTMLAttributes<HTMLLIElement> & MultiStepFormStepperItemProps
+>(({ step, title, className, ...props }, ref) => {
+  const { step: currentStep, max } = useMultiStepForm();
+  const hideLine = step === max;
+  const complete = step < currentStep;
+
+  return (
+    <li
+      ref={ref}
+      className={cn('flex-1 flex flex-col justify-center items-center text-center gap-2', className)}
+      {...props}
+    >
+      <div className="relative w-full center">
+        {!hideLine && (
+          <span
+            className={cn(
+              'h-[3px] -translate-y-[1.5px] w-full absolute top-1/2 left-1/2',
+              complete && 'bg-primary-dark dark:bg-primary-light',
+              !complete && 'bg-achromatic-light dark:bg-achromatic-dark',
+            )}
+          />
+        )}
+
+        <span
+          className={cn(
+            'w-9 h-9 rounded-full text-xl font-semibold flex justify-center items-center text-center z-[1]',
+            complete && 'bg-primary-dark dark:bg-primary-light text-achromatic-light dark:text-achromatic-dark',
+            !complete && 'bg-achromatic-light dark:bg-achromatic-dark text-primary-dark dark:text-primary-light',
+          )}
+        >
+          {step}
+        </span>
+      </div>
+
+      {!!title && <p className="text-primary-dark dark:text-primary-light">{title}</p>}
+    </li>
+  );
+});
+MultiStepFormStepperItem.displayName = 'MultiStepFormStepperItem';
+
 export {
   useMultiStepForm,
   MultiStepForm,
   MultiStepFormContent,
   Step,
+  MultiStepFormStepper,
+  MultiStepFormStepperItem,
 };
