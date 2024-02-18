af6cf1a51ff5c89d67ecd5095a6276d6ba4efae8
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-02-18 18:04:21 +0000

    cleaned up multistep form components usage in LoginPage

--- src/features/tempForm/setupForm.ts
+++
@@ -1,6 +0,0 @@
-import { createMultiStepForm } from '@/ui/MultiStepForm';
-import { FormSchemaType } from '@/features/tempForm/schema';
-
-type StepTitle = 'form1' | 'form2' | 'form3';
-
-export const { MultiStepForm, useMultiStepForm } = createMultiStepForm<FormSchemaType, StepTitle>();
