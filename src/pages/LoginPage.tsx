a63a21e2cfb912cb41dfc4d2982045863914de18
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-02-19 01:41:14 +0000

    minor refactor

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,28 +1,10 @@
-import { Form1 } from '@/features/tempForm/components/Form1';
-import { Form2 } from '@/features/tempForm/components/Form2';
-import { Form3 } from '@/features/tempForm/components/Form3';
-import {
-  MultiStepForm,
-  MultiStepFormItems,
-  MultiStepFormItem,
-  MultiStepFormStepper,
-  MultiStepFormStepperItem,
-} from '@/ui/MultiStepForm';
+import { Title } from '@/features/title/components/Title';
 
 export default function LoginPage() {
   return (
-    <MultiStepForm min={1} max={3}>
-      <MultiStepFormStepper>
-        <MultiStepFormStepperItem step={1} title="Form 1" />
-        <MultiStepFormStepperItem step={2} title="Form 2" />
-        <MultiStepFormStepperItem step={3} title="Form 3" />
-      </MultiStepFormStepper>
-
-      <MultiStepFormItems className="flex justify-center items-start">
-        <MultiStepFormItem step={1}><Form1 /></MultiStepFormItem>
-        <MultiStepFormItem step={2}><Form2 /></MultiStepFormItem>
-        <MultiStepFormItem step={3}><Form3 /></MultiStepFormItem>
-      </MultiStepFormItems>
-    </MultiStepForm>
+    <>
+      <Title title="Login" />
+      <div>LoginPage</div>
+    </>
   );
 }
