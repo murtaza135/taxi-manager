22f381ab15c403ba2c7fcfa738ee10b7de51d91a
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-02-19 18:01:51 +0000

    added Skeleton

--- src/pages/DriversPage.tsx
+++ src/pages/DriversPage.tsx
@@ -1,11 +1,19 @@
 import { Title } from '@/features/title/components/Title';
 import { Spinner } from '@/ui/Spinner';
+import { Skeleton } from '@/ui/Skeleton';
 
 export default function DriversPage() {
   return (
     <div className="center">
       <Title title="Drivers" />
-      <Spinner className="w-20 h-20" />
+      {/* <Spinner className="w-20 h-20" /> */}
+      {/* <div className="flex gap-4 items-center">
+        <Skeleton className="rounded-full w-10 h-10" />
+        <div className="space-y-2">
+          <Skeleton className="w-32 h-4" />
+          <Skeleton className="w-24 h-4" />
+        </div>
+      </div> */}
     </div>
   );
 }
