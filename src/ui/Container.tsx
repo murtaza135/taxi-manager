d2e06383f0f6ab4fbd9cff6027486dba9d871b37
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-02-19 16:53:57 +0000

    minor bug fixes

--- src/ui/Container.tsx
+++ src/ui/Container.tsx
@@ -21,8 +21,10 @@ type ContentContainerProps = {
 
 function ContentContainer({ className, children }: ContentContainerProps) {
   return (
-    <div className={cn('flex flex-col [&>*]:flex-grow w-full max-w-screen-2xl min-h-dvh mx-auto pl-5 sm:pl-[9.625rem] md:pl-[18.5rem] pr-5 pt-20 pb-5', className)}>
-      {children}
+    <div className={cn('w-full max-w-screen-2xl min-h-dvh mx-auto pl-5 sm:pl-[9.625rem] md:pl-[18.5rem] pr-5 pt-20 pb-5', className)}>
+      <div className="flex flex-col [&>*]:flex-grow w-full min-h-dvh overflow-x-hidden">
+        {children}
+      </div>
     </div>
   );
 }
