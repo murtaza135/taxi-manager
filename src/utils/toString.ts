dbcef1709ed1fe6668bf9f8faff1d0c445e31554
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-05-14 16:29:48 +0100

    minor change

--- src/utils/toString.ts
+++
@@ -1,5 +0,0 @@
-export function toString(value: unknown, defaultValue?: string) {
-  if (typeof value === 'string') return value;
-  if (typeof defaultValue === 'string') return defaultValue;
-  return String(value);
-}
