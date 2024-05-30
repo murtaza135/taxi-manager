779298606208c6b0b0aa8af28b782f59c58697ec
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-05-30 20:57:18 +0100

    private and public routes redirect to login page or home page depending on session

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -1,12 +1,39 @@
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
 import { SideNav } from '@/features/navigation/components/side-nav/SideNav';
 import { TopNav } from '@/features/navigation/components/top-nav/TopNav';
 import { MobileNav } from '@/features/navigation/components/mobile-nav/MobileNav';
 import { ContentContainer } from '@/ui/Container';
 import { Spinner } from '@/ui/Spinner';
+import { useSession } from '@/features/auth/hooks/useSession';
 
 export function PrivateLayout() {
+  const queryClient = useQueryClient();
+  const { isLoading, isError } = useSession();
+
+  useEffect(() => {
+    if (isError) queryClient.clear();
+  }, [isError, queryClient]);
+
+  if (isLoading) {
+    return (
+      <>
+        <SideNav />
+        <TopNav />
+        <ContentContainer>
+          <Spinner />
+        </ContentContainer>
+        <MobileNav />
+      </>
+    );
+  }
+
+  // TODO only redirect if auth error, not other error
+  if (isError) {
+    return <Navigate to="/login" replace />;
+  }
+
   return (
     <>
       <SideNav />
--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,21 +1,34 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { Spinner } from '@/ui/Spinner';
 import { BasicContainer } from '@/ui/Container';
 import { SimpleTopNav } from '@/features/navigation/components/top-nav/SimpleTopNav';
 import { useSession } from '@/features/auth/hooks/useSession';
 
 export function PublicLayout() {
-  const { data, error, status } = useSession();
-  console.log(data);
-  console.log(error);
-  console.log(status);
+  const { isLoading, isSuccess } = useSession();
+
+  if (isLoading) {
+    return (
+      <>
+        <SimpleTopNav />
+        <BasicContainer>
+          <Spinner />
+        </BasicContainer>
+      </>
+    );
+  }
+
+  // if there is a session, then we do not want to be in the public layout
+  if (isSuccess) {
+    return <Navigate to="/" replace />;
+  }
 
   return (
     <>
       <SimpleTopNav />
       <BasicContainer>
-        <Suspense fallback={<Spinner className="w-28 h-28" />}>
+        <Suspense fallback={<Spinner className="w-28 h-28 after:w-28 after:h-28" />}>
           <Outlet />
         </Suspense>
       </BasicContainer>
