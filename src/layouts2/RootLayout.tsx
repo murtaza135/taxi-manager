0e1a1ee855726f463a0e57b0def9cf43ff8bca03
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-06-18 03:32:01 +0100

    created RootLayout

--- src/layouts2/RootLayout.tsx
+++ src/layouts2/RootLayout.tsx
@@ -1,13 +1,39 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
+import { Suspense } from 'react';
 import { Toaster } from '@/ui/toast';
 import { ScrollToTopButton } from '@/features/scroll/ScrollToTopButton';
 
-export function RootLayout() {
+function rootLoader() {
+
+}
+
+function RootErrorBoundary() {
+  return <div>RootErrorBoundary</div>;
+}
+
+function RootSuspenseFallback() {
+  return <div>RootSuspenseFallback</div>;
+}
+
+function RootComponent() {
+  const { state } = useNavigation();
+
   return (
     <>
+      {(state === 'idle' || state === 'submitting') && <p className="fixed top-0 left-0">Loading...</p>}
       <Outlet />
       <Toaster />
       <ScrollToTopButton />
     </>
+  );
+}
+
+export const loader = rootLoader;
+export const ErrorBoundary = RootErrorBoundary;
+export function Component() {
+  return (
+    <Suspense fallback={<RootSuspenseFallback />}>
+      <RootComponent />
+    </Suspense>
   );
 }
