04534e1e4f3488d651c0ce94420e0e4513213fd4
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-06-18 21:56:24 +0100

    minor cleanup of router

--- src/config/router/router2.tsx
+++ src/config/router/router2.tsx
@@ -6,15 +6,27 @@ import * as privateLayoutOptions from '@/routes/layouts/private';
 import * as publicOnlyLayoutOptions from '@/routes/layouts/publicOnly';
 
 const routes = createRoutesFromElements(
-  <Route path="/" {...createRouteOptions(rootOptions, queryClient)}>
-    <Route {...createRouteOptions(publicOnlyLayoutOptions, queryClient)}>
+  <Route
+    id="root"
+    path="/"
+    {...createRouteOptions(rootOptions, queryClient)}
+  >
+    <Route
+      id="publicOnlyLayout"
+      {...createRouteOptions(publicOnlyLayoutOptions, queryClient)}
+    >
       <Route
+        id="driverPage"
         path="/driver"
         lazy={createRouteOptions(() => import('@/routes/pages/driver'), queryClient)}
       />
     </Route>
-    <Route {...createRouteOptions(privateLayoutOptions, queryClient)}>
+    <Route
+      id="privateLayout"
+      {...createRouteOptions(privateLayoutOptions, queryClient)}
+    >
       <Route
+        id="driver2Page"
         path="/driver2"
         lazy={createRouteOptions(() => import('@/routes/pages/driver'), queryClient)}
       />
