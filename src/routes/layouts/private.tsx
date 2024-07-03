50ab5a161fc2f41907089bafa22b192dda34369b
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-07-03 02:13:30 +0100

    cleane up private layout

--- src/routes/layouts/private.tsx
+++ src/routes/layouts/private.tsx
@@ -7,13 +7,10 @@ import { getLocalSession } from '@/features/auth/hooks/useLocalSession';
 import { AppError } from '@/errors/AppError';
 import { logout } from '@/features/auth/hooks/useLogout';
 import { toast } from '@/ui/toast';
-import { BasicContainer, ContentContainer } from '@/ui/Container';
-import { Spinner } from '@/ui/Spinner';
+import { ContentContainer } from '@/ui/Container';
 import { TopNav } from '@/features/navigation/components/top-nav/TopNav';
 import { SideNav } from '@/features/navigation/components/side-nav/SideNav';
 import { MobileNav } from '@/features/navigation/components/mobile-nav/MobileNav';
-import { GeneralErrorUI } from '@/errors/components/GeneralErrorUI';
-import { SimpleTopNav } from '@/features/navigation/components/top-nav/SimpleTopNav';
 
 const privateLayoutLoader = (queryClient: QueryClient) => async () => {
   const localSession = getLocalSession();
@@ -56,25 +53,6 @@ const privateLayoutLoader = (queryClient: QueryClient) => async () => {
   return null;
 };
 
-function PrivateLayoutSuspenseBoundary() {
-  return (
-    <BasicContainer center>
-      <Spinner />
-    </BasicContainer>
-  );
-}
-
-function PrivateLayoutErrorBoundary() {
-  return (
-    <>
-      <SimpleTopNav />
-      <BasicContainer center>
-        <GeneralErrorUI />
-      </BasicContainer>
-    </>
-  );
-}
-
 function PrivateLayoutComponent() {
   return (
     <>
@@ -89,6 +67,4 @@ function PrivateLayoutComponent() {
 }
 
 export const loader = privateLayoutLoader;
-export const SuspenseBoundary = PrivateLayoutSuspenseBoundary;
-export const ErrorBoundary = PrivateLayoutErrorBoundary;
 export const Component = PrivateLayoutComponent;
