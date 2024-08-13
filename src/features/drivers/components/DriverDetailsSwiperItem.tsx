d8605c5a3279e2b82b8be10aa59d7c2a6c0a3c39
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-08-13 17:07:16 +0100

    working on adding EditableInput to drivers details page

--- src/features/drivers/components/DriverDetailsSwiperItem.tsx
+++ src/features/drivers/components/DriverDetailsSwiperItem.tsx
@@ -21,9 +21,9 @@ export function DriverDetailsSwiperItem() {
     <Accordion type="multiple">
       <AccordionItem value="details">
         <AccordionNonCollapsibleContent>
-          <div className="flex items-center gap-6 flex-col xs:flex-row xs:items-start">
-            <div>
-              <Avatar className="w-full h-full max-w-40 max-h-40">
+          <div className="flex justify-start items-start gap-8 xs:gap-10 sm:gap-14 flex-col xs:flex-row py-3 px-2">
+            <div className="flex flex-col justify-start items-start gap-4 xs:max-w-48">
+              <Avatar className="w-full h-full max-w-24 max-h-24 xs:max-w-40 xs:max-h-40">
                 {data.picture_src && (
                   <AvatarImage
                     src={data.picture_src}
@@ -34,11 +34,24 @@ export function DriverDetailsSwiperItem() {
                   {extractInitials(data.name)}
                 </AvatarFallback>
               </Avatar>
+              <EditableInput defaultValue={capitalizeEachWord(data.name)} className="text-2xl font-bold" onSave={(value) => console.log(value)} />
             </div>
 
-            <div className="space-y-4 flex-grow w-full xs:w-fit">
-              <h2 className="text-2xl font-bold text-center xs:text-start">{capitalizeEachWord(data.name)}</h2>
-              <div>
+            <div className="flex gap-8 flex-wrap flex-grow overflow-hidden [&_input]:w-fit">
+              <div className="space-y-2">
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
+                <EditableInput title="lol" defaultValue="hehehedasdasdsadasdasdasdasdad" onSave={(value) => console.log(value)} />
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
+              </div>
+              <div className="space-y-2">
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
+                <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
                 <EditableInput title="lol" defaultValue="hehehe" onSave={(value) => console.log(value)} />
               </div>
             </div>
--- src/ui/form/Input.tsx
+++ src/ui/form/Input.tsx
@@ -294,20 +294,23 @@ const EditableInput = React.forwardRef<
       {title && (
         <p className="font-semibold text-sm text-achromatic-dark/65 dark:text-achromatic-500">{title}</p>
       )}
-      <div className={cn('flex justify-between items-center gap-2 border-achromatic-dark/65 dark:border-achromatic-500', !readOnly && 'border-b')}>
-        <input
+      <div className={cn('flex justify-between items-center gap-2 border-achromatic-dark/65 dark:border-achromatic-500 relative', !readOnly && 'border-b')}>
+        {/* <input
           {...props}
           ref={innerRef}
-          className={cn('w-full min-w-4 outline-none bg-transparent placeholder:text-achromatic-dark/65 dark:placeholder:text-achromatic-500 file:hidden', readOnly && 'cursor-default', !readOnly && 'cursor-auto', className)}
+          className={cn('w-full min-w-4 pr-7 pl-0 xs:pl-0 outline-none bg-transparent placeholder:text-achromatic-dark/65 dark:placeholder:text-achromatic-500 file:hidden', readOnly && 'cursor-default', !readOnly && 'cursor-auto', className)}
           placeholder={props.placeholder || 'N/A'}
           readOnly={readOnly}
           onKeyUp={(event) => (event.key === 'Enter' && handleSave())}
-        />
+        /> */}
+        <p className={cn('w-full min-w-4 pr-7 pl-0 xs:pl-0 outline-none bg-transparent placeholder:text-achromatic-dark/65 dark:placeholder:text-achromatic-500 file:hidden', readOnly && 'cursor-default', !readOnly && 'cursor-auto', className)}>
+          {props.value ?? props.defaultValue ?? 'N/A'}
+        </p>
         {readOnly
           ? (
             <Button
               variant="ghost"
-              className="p-0 transition-opacity opacity-0 group-hover:opacity-100"
+              className="p-0 transition-opacity opacity-0 group-hover:opacity-100 absolute right-0"
               onClick={handleEdit}
             >
               <MdModeEdit className="text-lg" />
@@ -316,7 +319,7 @@ const EditableInput = React.forwardRef<
           : (
             <Button
               variant="ghost"
-              className="p-0"
+              className="p-0 hover:opacity-100 absolute right-0"
               onClick={handleSave}
             >
               <IoMdCheckmark className="text-xl" />
