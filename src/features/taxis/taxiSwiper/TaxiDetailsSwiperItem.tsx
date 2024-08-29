4318938ccf8c1c6e5e8330d82c4ff5c6b65e3a69
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-08-29 16:58:43 +0100

    created TaxiDetailsSwiperItem template

--- src/features/taxis/taxiSwiper/TaxiDetailsSwiperItem.tsx
+++ src/features/taxis/taxiSwiper/TaxiDetailsSwiperItem.tsx
@@ -1,5 +1,43 @@
+import { Suspense } from 'react';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionCollapsibleContent,
+} from '@/ui/Accordion';
+import { Separator } from '@/ui/Separator';
+import { Spinner } from '@/ui/Spinner';
+
 export function TaxiDetailsSwiperItem() {
   return (
-    <div>TaxiDetailsSwiperItem</div>
+    <Accordion type="multiple" defaultValue={['details']}>
+      <AccordionItem value="details">
+        <AccordionTrigger>Taxi Details</AccordionTrigger>
+        <AccordionCollapsibleContent>
+          <Separator className="bg-achromatic-light dark:bg-achromatic-darker mb-3" />
+          <div>Taxi Details</div>
+        </AccordionCollapsibleContent>
+      </AccordionItem>
+
+      <AccordionItem value="taxiLicence">
+        <AccordionTrigger>Taxi Licence</AccordionTrigger>
+        <AccordionCollapsibleContent className="min-h-48 flex flex-col">
+          <Separator className="bg-achromatic-light dark:bg-achromatic-darker mb-3" />
+          <Suspense fallback={<Spinner />}>
+            <div>Taxi Licence</div>
+          </Suspense>
+        </AccordionCollapsibleContent>
+      </AccordionItem>
+
+      <AccordionItem value="insurance">
+        <AccordionTrigger>Insurance</AccordionTrigger>
+        <AccordionCollapsibleContent className="min-h-48 flex flex-col">
+          <Separator className="bg-achromatic-light dark:bg-achromatic-darker mb-3" />
+          <Suspense fallback={<Spinner />}>
+            <div>Insurance</div>
+          </Suspense>
+        </AccordionCollapsibleContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
