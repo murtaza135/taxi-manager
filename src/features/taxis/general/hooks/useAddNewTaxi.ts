aba444a8df581aa27943467e5f12206d269d2c17
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-09-02 16:10:54 +0100

    removed compression from useAddNewTaxi

--- src/features/taxis/general/hooks/useAddNewTaxi.ts
+++ src/features/taxis/general/hooks/useAddNewTaxi.ts
@@ -7,7 +7,6 @@ import { SupabaseError } from '@/errors/classes/SupabaseError';
 import { queryClient as globalQueryClient } from '@/config/api/queryClient';
 import { sessionOptions } from '@/features/auth/hooks/useSession';
 import { supabase } from '@/config/api/supabaseClient';
-import { compressImage } from '@/utils/compression/compressImage';
 import { extname } from '@/utils/path/extname';
 
 type TaxiDocumentPathsObject = {
@@ -22,99 +21,74 @@ export async function addNewTaxi(formData: AddNewTaxiTransformedSchema) {
   const session = await globalQueryClient.ensureQueryData(sessionOptions());
   const documentPaths: TaxiDocumentPathsObject = {};
   const {
-    picture_path,
-    logbook_document_path,
-    compliance_certificate_document_path,
-    phc_licence_document_path,
-    insurance_document_path,
+    picture_path: picture,
+    logbook_document_path: logbook_document,
+    compliance_certificate_document_path: compliance_certificate_document,
+    phc_licence_document_path: phc_licence_document,
+    insurance_document_path: insurance_document,
     ...nonFileFormData
   } = formData;
 
   // TODO add storage rollback on error
-  if (picture_path) {
-    const compressedPicture = await compressImage(
-      picture_path,
-      { maxWidth: 150, maxHeight: 150 },
-    );
-
-    const fileName = `${session.user.id}/taxi-pictures/${uuidv4()}${extname(compressedPicture.name)}`;
+  if (picture) {
+    const fileName = `${session.user.id}/taxi-pictures/${uuidv4()}${extname(picture.name)}`;
 
     const { data: storageData } = await supabase
       .storage
       .from('main')
-      .upload(fileName, compressedPicture, { upsert: true });
+      .upload(fileName, picture, { upsert: true });
 
     if (storageData) {
       documentPaths.picture_path = storageData.path;
     }
   }
 
-  if (logbook_document_path) {
-    const compressedLogbookDocument = await compressImage(
-      logbook_document_path,
-      { maxWidth: 500, maxHeight: 500 },
-    );
-
-    const fileName = `${session.user.id}/logbooks/${uuidv4()}${extname(compressedLogbookDocument.name)}`;
+  if (logbook_document) {
+    const fileName = `${session.user.id}/logbooks/${uuidv4()}${extname(logbook_document.name)}`;
 
     const { data: storageData } = await supabase
       .storage
       .from('main')
-      .upload(fileName, compressedLogbookDocument, { upsert: true });
+      .upload(fileName, logbook_document, { upsert: true });
 
     if (storageData) {
       documentPaths.logbook_document_path = storageData.path;
     }
   }
 
-  if (compliance_certificate_document_path) {
-    const compressedComplianceCertificateDocument = await compressImage(
-      compliance_certificate_document_path,
-      { maxWidth: 500, maxHeight: 500 },
-    );
-
-    const fileName = `${session.user.id}/taxi-badges/${uuidv4()}${extname(compressedComplianceCertificateDocument.name)}`;
+  if (compliance_certificate_document) {
+    const fileName = `${session.user.id}/taxi-badges/${uuidv4()}${extname(compliance_certificate_document.name)}`;
 
     const { data: storageData } = await supabase
       .storage
       .from('main')
-      .upload(fileName, compressedComplianceCertificateDocument, { upsert: true });
+      .upload(fileName, compliance_certificate_document, { upsert: true });
 
     if (storageData) {
       documentPaths.compliance_certificate_document_path = storageData.path;
     }
   }
 
-  if (phc_licence_document_path) {
-    const compressedPHCLicenceDocument = await compressImage(
-      phc_licence_document_path,
-      { maxWidth: 500, maxHeight: 500 },
-    );
-
-    const fileName = `${session.user.id}/phc-licences/${uuidv4()}${extname(compressedPHCLicenceDocument.name)}`;
+  if (phc_licence_document) {
+    const fileName = `${session.user.id}/phc-licences/${uuidv4()}${extname(phc_licence_document.name)}`;
 
     const { data: storageData } = await supabase
       .storage
       .from('main')
-      .upload(fileName, compressedPHCLicenceDocument, { upsert: true });
+      .upload(fileName, phc_licence_document, { upsert: true });
 
     if (storageData) {
       documentPaths.phc_licence_document_path = storageData.path;
     }
   }
 
-  if (insurance_document_path) {
-    const compressedInsuranceDocument = await compressImage(
-      insurance_document_path,
-      { maxWidth: 500, maxHeight: 500 },
-    );
-
-    const fileName = `${session.user.id}/insurance-documents/${uuidv4()}${extname(compressedInsuranceDocument.name)}`;
+  if (insurance_document) {
+    const fileName = `${session.user.id}/insurance-documents/${uuidv4()}${extname(insurance_document.name)}`;
 
     const { data: storageData } = await supabase
       .storage
       .from('main')
-      .upload(fileName, compressedInsuranceDocument, { upsert: true });
+      .upload(fileName, insurance_document, { upsert: true });
 
     if (storageData) {
       documentPaths.insurance_document_path = storageData.path;
