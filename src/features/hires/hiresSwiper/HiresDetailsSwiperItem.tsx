50b2423b582de4ac9e86bb78bc53c503392a130b
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-09-09 22:40:38 +0100

    created template for all swiper items for hires

--- src/features/hires/hiresSwiper/HiresDetailsSwiperItem.tsx
+++ src/features/hires/hiresSwiper/HiresDetailsSwiperItem.tsx
@@ -1,5 +1,14 @@
+import { Separator } from '@/ui/Separator';
+import { Card, CardTitle } from '@/ui/Card';
+
 export function HiresDetailsSwiperItem() {
   return (
-    <div>HiresDetailsSwiperItem</div>
+    <Card className="p-4 text-achromatic-dark dark:text-achromatic-lighter">
+      <CardTitle className="text-start text-base pb-4">Hire Details</CardTitle>
+      <div>
+        <Separator className="bg-achromatic-light dark:bg-achromatic-darker mb-3" />
+        <div>HireDetailsSection</div>
+      </div>
+    </Card>
   );
 }
--- src/features/hires/hiresSwiper/HiresTaxiSwiperItem.tsx
+++ src/features/hires/hiresSwiper/HiresTaxiSwiperItem.tsx
@@ -1,5 +1,14 @@
+import { Separator } from '@/ui/Separator';
+import { Card, CardTitle } from '@/ui/Card';
+
 export function HiresTaxiSwiperItem() {
   return (
-    <div>HiresTaxiSwiperItem</div>
+    <Card className="p-4 text-achromatic-dark dark:text-achromatic-lighter">
+      <CardTitle className="text-start text-base pb-4">Hired Taxi</CardTitle>
+      <div>
+        <Separator className="bg-achromatic-light dark:bg-achromatic-darker mb-3" />
+        <div>HiresTaxiSwiperItem</div>
+      </div>
+    </Card>
   );
 }
