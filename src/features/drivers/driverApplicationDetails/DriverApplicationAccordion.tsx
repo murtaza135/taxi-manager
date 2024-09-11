66a7e8cbcabe71861539af3aa60ba96c2dc5915a
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-09-11 02:06:35 +0100

    created DriverApplicationAccordion

--- src/features/drivers/driverApplicationDetails/DriverApplicationAccordion.tsx
+++ src/features/drivers/driverApplicationDetails/DriverApplicationAccordion.tsx
@@ -1,5 +1,46 @@
+import { Suspense } from 'react';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionCollapsibleContent,
+} from '@/ui/Accordion';
+import { Separator } from '@/ui/Separator';
+import { DriverApplicationDetailsSection } from '@/features/drivers/driverApplicationDetails/DriverApplicationDetailsSection';
+import { DriverApplicationLicenceDetailsSection } from '@/features/drivers/driverApplicationDetails/DriverApplicationLicenceDetailsSection';
+import { DriverApplicationTaxiBadgeDetailsSection } from '@/features/drivers/driverApplicationDetails/DriverApplicationTaxiBadgeDetailsSection';
+import { Spinner } from '@/ui/Spinner';
+
 export function DriverApplicationAccordion() {
   return (
-    <div>DriverApplicationAccordion</div>
+    <Accordion type="multiple" defaultValue={['details']} className="!flex-grow-0">
+      <AccordionItem value="details">
+        <AccordionTrigger>Driver Details</AccordionTrigger>
+        <AccordionCollapsibleContent>
+          <Separator className="bg-achromatic-light dark:bg-achromatic-darker mb-3" />
+          <DriverApplicationDetailsSection />
+        </AccordionCollapsibleContent>
+      </AccordionItem>
+
+      <AccordionItem value="driversLicence">
+        <AccordionTrigger>Drivers Licence</AccordionTrigger>
+        <AccordionCollapsibleContent className="flex flex-col">
+          <Separator className="bg-achromatic-light dark:bg-achromatic-darker mb-3" />
+          <Suspense fallback={<Spinner />}>
+            <DriverApplicationLicenceDetailsSection />
+          </Suspense>
+        </AccordionCollapsibleContent>
+      </AccordionItem>
+
+      <AccordionItem value="taxiBadge">
+        <AccordionTrigger>Taxi Badge</AccordionTrigger>
+        <AccordionCollapsibleContent className="flex flex-col">
+          <Separator className="bg-achromatic-light dark:bg-achromatic-darker mb-3" />
+          <Suspense fallback={<Spinner />}>
+            <DriverApplicationTaxiBadgeDetailsSection />
+          </Suspense>
+        </AccordionCollapsibleContent>
+      </AccordionItem>
+    </Accordion>
   );
 }
