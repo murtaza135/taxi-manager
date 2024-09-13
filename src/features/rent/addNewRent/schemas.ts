420e746a538ba1ac60a30dd9ab06d4a7ffa5b798
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2024-09-13 15:01:02 +0100

    finished schema for adding new hire

--- src/features/rent/addNewRent/schemas.ts
+++ src/features/rent/addNewRent/schemas.ts
@@ -1,25 +1,56 @@
 import { z } from 'zod';
+import { isBefore, isValid } from 'date-fns';
+import isCurrency from 'validator/es/lib/isCurrency';
+import { mapValues } from 'lodash';
+import { MergeOverwrite } from '@/types/utils';
 
-export const addNewRentSchema = z.object({
+export const addNewRentHireSchema = z.object({
   hire_id: z
     .number({ required_error: 'Taxi required' })
     .positive('Taxi required'),
-  number_plate: z
-    .string({ required_error: 'Number plate required' })
-    .trim()
-    .toUpperCase()
-    .min(1, 'Number plate required')
-    .transform((val) => val.replace(/\s/g, '')),
-  phc_number: z
-    .string()
-    .trim()
-    .toUpperCase()
-    .optional()
-    .or(z.literal('')),
-  driver_name: z
-    .string({ required_error: 'Driver required' })
-    .trim()
-    .min(1, 'Driver required'),
 });
 
+export const addNewRentDetailsSchema = z.object({
+  start_date: z
+    .string({ required_error: 'Start date required' })
+    .refine((val) => isValid(new Date(val)), { message: 'Invalid start date' }),
+  end_date: z
+    .string({ required_error: 'End date required' })
+    .refine((val) => isValid(new Date(val)), { message: 'Invalid end date' }),
+  amount: z
+    .string({ required_error: 'Rent amount required' })
+    .trim()
+    .min(1, 'Rent amount required')
+    .refine((val) => isCurrency(val)),
+}).refine(
+  ({ start_date, end_date }) => {
+    if (end_date) {
+      return isBefore(start_date, end_date);
+    }
+    return true;
+  },
+  {
+    message: 'End date must come after start date',
+    path: ['end_date'],
+  },
+);
+
+export const addNewRentSchema = addNewRentHireSchema
+  .merge(addNewRentDetailsSchema.innerType());
+
+export const addNewRentTransformer = (data: AddNewRentSchema) => (
+  mapValues(data, (val, key) => {
+    if (!val) return undefined;
+    if (Number.isNaN(val)) return undefined;
+    if (key === 'amount') return Number(val);
+    return val;
+  }) as AddNewRentTransformedSchema
+);
+
+export type AddNewRentHireSchema = z.infer<typeof addNewRentHireSchema>;
+export type AddNewRentDetailsSchema = z.infer<typeof addNewRentDetailsSchema>;
 export type AddNewRentSchema = z.infer<typeof addNewRentSchema>;
+
+export type AddNewRentTransformedSchema = MergeOverwrite<AddNewRentSchema, {
+  amount: number;
+}>;
